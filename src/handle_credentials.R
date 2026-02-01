2965ba90f73befcc6027abe2b90a93f2002e443e
thelonius182 <lcavdakker@gmail.com>
2026-02-01 16:03:57 +0100

    refactor M2M-job backup cred-experiment

--- src/handle_credentials.R
+++ src/handle_credentials.R
@@ -1,38 +1,17 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# USAGE: in a terminal run this cmd
-#        G:\R_proj_gergiev\released\salsa_toolbox_r>Rscript ./src/handle_credentials.R -p <the password>
-# This will store the encrypted password (cyphertext) in C:/Users/gergiev/cz_rds_store/cpnm_creds.RDS, along
-# with the key and nonce needed to decrypt it.
-#
-# To use the plaintext pwd in a script, see e.g. here:
-#        G:/R_proj_gergiev/released/salsa_toolbox_r/src/fiddle_cpnm_db.R
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-pacman::p_load(openssl, optparse, readr)
+pacman::p_load(sodium)
 
-# Define the options
-moro_options <- list(
-  make_option(c("-p", "--password"), type = "character", default = NULL,
-              help = "the password to be encrypted", metavar = "character")
-)
+# Your password
+password <- "mySecretPassword"
 
-# Parse the command-line arguments
-opt_parser <- OptionParser(option_list = moro_options)
-opts <- parse_args(opt_parser)
+# Generate a random key (keep this secret)
+key <- bin2hex(random(32))
 
-# Check if required arguments are provided
-if (is.null(opts$password)) {
-  stop("missing argument(s). Please provide the password, using '-p' or '--password'.", call. = FALSE)
-}
+# Encrypt the password
+password_bytes <- charToRaw(password)
+encrypted <- simple_encrypt(password_bytes, key = hex2bin(key))
 
-# one-time set up
-key <- rand_bytes(32)  # 256-bit key
-iv  <- rand_bytes(16)
-msg <- charToRaw(opts$password)
-msg_enc <- aes_cbc_encrypt(msg, key = key, iv = iv)
-write_rds(list(key = key, iv = iv, msg_enc = msg_enc), file = "C:/Users/gergiev/cz_rds_store/cpnm_creds.RDS")
+# Decrypt
+decrypted <- rawToChar(simple_decrypt(encrypted, key = hex2bin(key)))
 
-# validate
-cpnm_creds <- read_rds(file = "C:/Users/gergiev/cz_rds_store/cpnm_creds.RDS")
-msg_dec <- aes_cbc_decrypt(cpnm_creds$msg_enc, key = cpnm_creds$key, iv = cpnm_creds$iv)
-rawToChar(msg_dec)
-
+encrypted
+decrypted  # Should return "mySecretPassword"
--- src/muw_audio2mac.R
+++ src/muw_audio2mac.R
@@ -1,17 +1,53 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # Verplaats bestelde audio naar MAC en pak uit
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-pacman::p_load(readr, futile.logger, dplyr, yaml, fs, magrittr, lubridate, zip, stringr)
-print(paste0("audio mover job ", now()))
-src_audio <- dir_ls(path = "C:/Users/gergiev/Downloads/", type = "file", regexp = "Bestelling#.*\\.zip") 
+pacman::p_load(readr, futile.logger, dplyr, yaml, fs, magrittr, lubridate, zip, stringr, rlang)
 
-stopifnot("no error, just stopping as there are no files to move & unpack" = length(src_audio) > 0)
+fa <- flog.appender(appender.file("g:/salsa/Logs/muw_audio2mac.log"), name = "m2m_log")
+flog.info("Running Audio-mover", name = "m2m_log")
 
-for (f1 in src_audio) {
-  src_audio_file <- path_file(f1)
-  tgt_audio <- path_join(c("//uitzendmac-2/Data/Nipper/muziekweb_audio/", src_audio_file))
-  file_move(f1, tgt_audio)
-  command <- paste0("7z.exe x ", tgt_audio, " -o", path_dir(tgt_audio), " -y")
-  system(command)
-  file_delete(tgt_audio)
+# start MCL
+repeat { 
+  
+  src_audio <- dir_ls(path = "C:/Users/gergiev/Downloads/", type = "file", regexp = "Bestelling#.*\\.zip") 
+
+  if (length(src_audio) == 0) {
+    flog.info("No MuW-zips found.", name = "m2m_log")
+    break
+  }
+  
+  for (f1 in src_audio) {
+    src_audio_file <- path_file(f1)
+    flog.info("Found %s", src_audio_file, name = "m2m_log")
+    tryCatch(
+      {
+        tgt_audio <- path_join(c("//uitzendmac-2/Data/Nipper/muziekweb_audio/", src_audio_file))
+        file_move(f1, tgt_audio)
+      },
+      error = function(e1) {
+        flog.info("Moving to Mac failed: %s", conditionMessage(e1), name = "m2m_log")
+        break
+      }
+    )
+    
+    tryCatch(
+      {
+        cmd  <- "C:/Program Files/7-Zip/7z.exe"
+        args <- c("x", tgt_audio, paste0("-o", path_dir(tgt_audio)), "-y", "-bb3")
+        out  <- system2(cmd, args, stdout = TRUE, stderr = TRUE)
+        status <- attr(out, "status") %||% 0
+        out_msg <- paste0(paste(out, collapse = "\n"), "\nEXIT=", status, "\n")
+        flog.info(out_msg, name = "m2m_log")
+      },
+      error = function(e1) {
+        flog.info("Unpacking on Mac failed: %s", conditionMessage(e1), name = "m2m_log")
+        break
+      }
+    )
+    
+    file_delete(tgt_audio)
+  }
+  
+  # leave MCL
+  break
 }
